83a3b5d502d817f50e645f797da23c38ee0c57c5
yebin048 <lyna0408@naver.com>
2025-11-23 17:50:58 +0900

    2

--- main.c
+++ main.c
@@ -2,25 +2,57 @@
 #include <stdlib.h>
 #include <time.h>
 #include "board.h"
+
+#define MAX_DIE		6
+
+int rolldie (void)
+{
+	return rand() %MAX_DIE+1;
+}
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int cnt;
+	int pos;
+	int coinResult;
+
 	srand((unsigned) (time (NULL)) );
 	
 	//opening
-	printf("===============================================\n");
-	printf("===@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@===\n");
-	printf("===			SHARK ISLAND GAME START			===\n");
-	printf("===@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@===\n");
-	printf("===============================================\n");
+	printf("===================================================\n");
+	printf("===@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@===\n");
+	printf("================SHARK ISLAND GAME START============\n");
+	printf("===@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@===\n");
+	printf("===================================================\n");
+
+	//step.1
+	board_initBoard();
+	
+	//step.2
+	cnt=0;
+	pos=0;
+	coinResult=0;
+
+	do{
+		int die_result;
+		board_printBoardStatus();
+		die_result = rolldie();
+		pos+= die_result;
+		printf("pos: %i (die:%i)\n", pos, die_result);
+		coinResult += board_getBoardCoin(pos);
+		printf("coin: %i\n", coinResult);
+		cnt++;
+	} while(cnt<5);
+
 
 	//ending
 	printf("\n\n\n\n\n\n\n\n\n\n");
-	printf("===============================================\n");
-	printf("===@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@===\n");
-	printf("===			SHARK ISLAND GAME END			===\n");
-	printf("===@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@===\n");
-	printf("===============================================\n\n");
+	printf("===================================================\n");
+	printf("===@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@===\n");
+	printf("================SHARK ISLAND GAME END==============\n");
+	printf("===@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@+@===\n");
+	printf("===================================================\n\n");
 	
 	system("PAUSE");
 	return 0;
